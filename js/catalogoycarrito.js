586172631dda3317354f335da2169db37c988600
Lucaguillen <Lucaguillen@icloud.com>
2023-06-08 23:28:07 -0300

    carrito funcionando

--- js/catalogoycarrito.js
+++ js/catalogoycarrito.js
@@ -61,9 +61,42 @@ card.innerHTML=
 `
 catalogoCaptura.appendChild(card)
 });
-/* FUNCION QUE MANDA LA CARD AL CARRITO */
+/* GENERA EL CARRITO */
 const carritoCaptura = document.getElementById("carrito")
+const h2CarritoCaptura = document.getElementById("h2carrito")
+const carritoDom = (i)=>{
+    if (carrito.length === 0){
+        carritoCaptura.className="carrito"
+        addCarrito(i)
+    }else{
+        addCarrito(i)
+    }
+}
+/* FUNCION QUE AÑADE PRODUCTOS */
+function limpiarCarritoHTML(){
+    carritoCaptura.innerHTML=`<h2 class="titulo">Carrito</h2>`
+}
+const addCarrito = (i)=>{
+    const productoSelect = carrito.findIndex((e)=>{
+        return e.id === catalogo[i].id
+    })
+    const addProducto = catalogo[i]
+    if ((productoSelect === -1) && (addProducto.stock !=0)){
+        addProducto.cantidad = 1
+        addProducto.stock -= 1
+        carrito.push(addProducto)
+        cardAlCarrito()    
+    }else if (addProducto.stock !=0){
+        carrito[productoSelect].cantidad += 1
+        carrito[productoSelect].stock -= 1   
+    }else{
+        alert("producto sin stock")
+    }   
+}
+
+/* FUNCION QUE MANDA LA CARD AL CARRITO */
 const cardAlCarrito = ()=>{
+    limpiarCarritoHTML()
     carrito.forEach((p,i)=>{
         let card = document.createElement("div");
         card.className= "card"
@@ -75,45 +108,12 @@ const cardAlCarrito = ()=>{
         <div class="detallesCard">
             <h2 class="tituloCard">${p.modelo}</h2>
             <p class="infoCard">Marca: ${p.marca}</p>
-            <p class="infoCard">Stock: ${p.stock}</p>
             <p class="precioCard">$ ${p.precio}</p>
-            <button class="removeCarrito" onClick="carritoDom(${i})">Eliminar del carrito</button>
+            <button class="removeCarrito" >Eliminar del carrito</button>
         </div>
         `
-        carritoCaptura.appendChild(card)  
+        carritoCaptura.appendChild(card) 
     });
 }
-/* FUNCION QUE AÑADE PRODUCTOS */
-const addCarrito = (i)=>{
-    const productoSelect = carrito.findIndex((e)=>{
-        return e.id === catalogo[i].id
-    })
-    const addProducto = catalogo[i]
-    if ((productoSelect === -1) && (addProducto.stock !=0)){
-        addProducto.cantidad = 1
-        addProducto.stock -= 1
-        carrito.push(addProducto)
-        cardAlCarrito()     
-    }else if (addProducto.stock !=0){
-        carrito[productoSelect].cantidad += 1
-        carrito[productoSelect].stock -= 1
-        cardAlCarrito()
-    }else{
-        alert("producto sin stock")
-    }   
-}
-/* GENERA EL CARRITO Y AÑADE PRODUCTOS */
-const contenedorDom = document.getElementById("contenedor")
-const carritoDom = (i)=>{
-    if (carrito.length === 0){
-        let divCarrito = document.createElement("div")
-        divCarrito.id= "carrito"
-        divCarrito.className="carrito"
-        divCarrito.innerHTML=`<h2 class="titulo">Carrito</h2>`
-        contenedorDom.appendChild(divCarrito)
-        addCarrito(i)
-    }else{
-        addCarrito(i)
-    }
-}
+
 
