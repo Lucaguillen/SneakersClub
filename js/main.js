39d01e87efa04bf9af6f91e390a8ed3dabf49d59
Lucaguillen <Lucaguillen@icloud.com>
2023-06-23 13:27:14 -0300

    ../json

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ let totalPages = 0
 let data = []
 let cart = []
 const fetchData = async () => {
-  const resp = await fetch("./json/data.json")
+  const resp = await fetch("../json/data.json")
   data = await resp.json()
   showCards()
 }
